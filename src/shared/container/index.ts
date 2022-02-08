b9f734f9326203c42e506a72f44c806648a0dd99
jcagz96 <jcagz@iscte-iul.pt>
2022-02-08 23:04:16 +0000

    added user tokens migration password controller

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,13 +5,18 @@ import '@shared/container/providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>('AppointmentRepository', AppointmentRepository);
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
-// container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
+
+container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
 
 
 
